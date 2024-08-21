f1f20f58a1d7f2606bce07df5bb9802c2dbedf66
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-21 22:55:42 +0530

    object

---
+++ variables/object.js
@@ -0,0 +1,18 @@
+let car = {
+    model :"2022",
+    make  :"Tata",
+    speed :"mege 200",
+}
+
+
+console.log(car)
+
+
+let bike = {
+    barnd : "hero",
+    owner : "ravi",
+    //price : "20202002",
+    color : "red and black",
+}
+
+console.log(bike)
