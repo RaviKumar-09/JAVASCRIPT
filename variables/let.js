9b275ed7319eb68b44feb17d70637f837e6aee11
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-23 14:50:51 +0530

    Redeclaring

--- variables/let.js
+++ variables/let.js
@@ -1,13 +1,23 @@
-var x = 2;
+v//ar x = 2;
 //let x = 3;
 
 console.log(x)
 
 {
-let x = 2;
+//let x = 2;
 //let x = 3;
 console.log(x)
 
+}
+
+let x = 2;
+
+{
+let x = 3;
+}
+
+{
+let x = 4;
 }
 
 
