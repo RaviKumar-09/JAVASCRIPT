c8dcac89f5a9c93fd20573807604cb0cbc7d9cb8
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-23 21:27:21 +0530

    JavaScript Data Types

---
+++ data Types/dt.js
@@ -0,0 +1,34 @@
+// Numbers
+
+let x = 19;
+let y = 1000
+console.log(x, y)
+
+//Strings
+
+let c = "chinnari";
+let r = "ravi";
+console.log(r , c)
+
+// Booleans
+
+
+let t = true;
+let f = false;
+console.log(t   ,   f)
+
+// Object
+
+const o = {ravi : "chinnari", susendra : "ribaka"};
+console.log(o);
+
+//Array Object
+
+const cars = ['valvo' ,'saab','bmw']
+console.log(cars)
+
+
+//Date Object
+
+const date = new Date ('2024-08-24')
+console.log(date)
