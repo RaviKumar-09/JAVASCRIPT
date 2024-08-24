4db768a098a0e4925120384b77e32148a2aa5cee
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-24 18:26:40 +0530

    if else

--- loops/if.js
+++ loops/if.js
@@ -1,3 +1,15 @@
-if (condition){
+/*if (condition){
     // Here the block of code excuted if the condition is true
+}*/
+
+
+let hour = 1;
+if (hour <= 18) {
+    console.log("Good Moring");
+}
+
+
+let car = "blue";
+if (car == "blue"){
+    console.log('Happy to buy a new car mye dear friend')
 }
