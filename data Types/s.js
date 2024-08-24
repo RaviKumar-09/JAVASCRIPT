f19bd40b8429c9317ea891c9adcedf6631256861
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-24 17:10:04 +0530

    JavaScript Strings

--- data Types/s.js
+++ data Types/s.js
@@ -3,3 +3,9 @@ console.log(typeof name);
 
 let selfName = 'Ainapudi Ravi Kumar';
 console.log(typeof selfName);
+
+let bio = "it's alright";
+console.log(bio);
+
+let sd = 'it"s my data from scratch';
+console.log(sd);
