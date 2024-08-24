02e541e3d9b52bfa0a9c8f4faaf0c48a21191c62
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-24 17:15:23 +0530

    JavaScript Numbers

---
+++ data Types/num.js
@@ -0,0 +1,13 @@
+let x1 = 100.1000;
+console.log(x1);
+
+let x2 = 143;
+console.log(x2);
+
+//Exponential Notation
+
+let x3 = 123e5;
+console.log(typeof x3);
+
+let x4 = 1234e-5;
+console.log(typeof x4);
