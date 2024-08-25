c0e513973b900128841af5b4664b03065431a518
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-25 23:23:25 +0530

    finding day by alarm using switch

---
+++ switch/day.js
@@ -0,0 +1,31 @@
+//let day = Week;
+let day1 = Sunday;
+let day2 = Monday;
+let day3 = Tuesday;
+let day4 = Wednesday;
+let day5 = Thursday;
+let day6 = Friday;
+let day7 = Saturday;
+
+switch(day1){
+    case day1:
+        console.log("Today is Sunday let's wake up at 10 am");
+        break;
+    case day2:
+        console.log("Today is Monday let's wake up at 4 am");
+        break;
+    case day3:
+        console.log("Today is Tuesday let's wake up at 5 am ");
+        break;
+    case day4:
+        console.log("Today is Wednesday lets wake up at 6 am");
+        break;
+    case day5:
+        console.log("Today is Thursday let's wake up at 7 am");
+        break;
+    case day6:
+        console.log("Today is Friday let's wake up at 8 am");
+        break;
+    case day7:
+        console.log("Today is Saturday let's wake up at 8 am")
+}
