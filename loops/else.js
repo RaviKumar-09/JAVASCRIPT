c54e4d04447445cbb937492ab2c043e646ef9361
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-25 22:13:42 +0530

    The else Statement

--- loops/else.js
+++ loops/else.js
@@ -5,3 +5,12 @@ if (fistName == "Ravi"){
 else{
     console.log("Ravi is not there")
 }
+
+
+let fist_Name = "Ravi"
+if (fist_Name == "Ravi"){
+    console.log("Hi Ravi how are You!")
+}
+else{
+    console.log("Ravi is not there")
+}
