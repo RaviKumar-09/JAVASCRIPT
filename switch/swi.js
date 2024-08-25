f283c8e235ce63c20d3adf83940e7d7e5fe52f5a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-25 23:02:08 +0530

    adding Y variable

--- switch/swi.js
+++ switch/swi.js
@@ -7,3 +7,14 @@ switch(x){
     default:
         console.log("we find the value of 'y'");
 }
+
+
+let x1 = 100;
+let y2 = 100;
+switch(y2){
+    case 100:
+        console.log("We find the value of 'x'")
+        break;
+    default:
+        console.log("we find the value of 'y'");
+}
