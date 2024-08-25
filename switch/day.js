6e963c79cc0d583e284e2402ed2f60e0045946dc
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-25 23:28:12 +0530

    day

--- switch/day.js
+++ switch/day.js
@@ -29,3 +29,35 @@ switch(day1){
     case day7:
         console.log("Today is Saturday let's wake up at 8 am")
 }
+
+//let day = Week;
+let day11 = "Sunday";
+let day22 = "Monday";
+let day33 = "Tuesday";
+let day44 = "Wednesday";
+let day55 = "Thursday";
+let day66 = "Friday";
+let day77= "Saturday";
+
+switch(day22){
+    case day11:
+        console.log("Today is Sunday let's wake up at 10 am");
+        break;
+    case day22:
+        console.log("Today is Monday let's wake up at 4 am");
+        break;
+    case day33:
+        console.log("Today is Tuesday let's wake up at 5 am ");
+        break;
+    case day44:
+        console.log("Today is Wednesday lets wake up at 6 am");
+        break;
+    case day55:
+        console.log("Today is Thursday let's wake up at 7 am");
+        break;
+    case day66:
+        console.log("Today is Friday let's wake up at 8 am");
+        break;
+    case day77:
+        console.log("Today is Saturday let's wake up at 8 am")
+}
