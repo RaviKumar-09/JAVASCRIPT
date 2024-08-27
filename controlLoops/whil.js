1a0f0450de859e7ec48ac9ba9ffdfc29e745bc28
RaviKumar <ravikumarainapudi97@gmail.com>
2024-08-27 22:08:22 +0530

    while loop

---
+++ controlLoops/whil.js
@@ -0,0 +1,15 @@
+
+
+let i = 1;
+
+while(i <= 10){
+    console.log("The number is " + i);
+    i++;
+}
+
+
+let num = 5;
+while(num <= 10){
+    console.log("Here the numbere we finf togther" + num);
+    num++;
+}
