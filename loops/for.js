58038e2dd3cab351ba3372562051f898b326ff9b
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-01 22:02:17 +0530

    i = 2

--- loops/for.js
+++ loops/for.js
@@ -9,3 +9,10 @@ for (let i = 0; i< cars.length; i++){
 for (let i = 0; i < 5; i++){
     console.log("The Numner " + i );
 }
+
+
+let i = 2;
+let len = cars.length;
+for(; i< len; i++){
+    console.log(cars[i])
+}
