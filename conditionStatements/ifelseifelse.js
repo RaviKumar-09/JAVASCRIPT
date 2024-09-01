6fb6cf16492e8de5096a951923723ac4e7b1fc31
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-01 21:47:54 +0530

    else if Statement

---
+++ conditionStatements/ifelseifelse.js
@@ -0,0 +1,11 @@
+let time = 6
+if(time <2){
+    msg = "Good Afternoon"
+    console.log(msg);
+}else if(time <=6){
+    msg = "Good Evening";
+    console.log(msg);
+}else{
+    msg =" Good nigth my dear friend go and sleep"
+    console.log(msg);
+}
