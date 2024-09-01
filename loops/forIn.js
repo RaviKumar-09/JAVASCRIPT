6d5a9896bcd2d9e6910a1b5ec048761188b7f683
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-01 22:51:41 +0530

    The JavaScript for in statement can also loop over the properties of an Array

--- loops/forIn.js
+++ loops/forIn.js
@@ -2,3 +2,11 @@ const person = {fName :"Ravi", lName :"Kumar", age :22};
 for (let i in person){
     console.log(person[i])
 }
+
+
+
+const array = [1,3,4,6,7,9,0,7,7,8,9,9,0,0]
+
+for (let i in array){
+    console.log(array[i])
+}
