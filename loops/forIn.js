ca65f99695b577a6b658a3d25fa61fc044580e60
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-01 23:08:10 +0530

    The forEach() method calls a function (a callback function) once for each array element.

--- loops/forIn.js
+++ loops/forIn.js
@@ -10,3 +10,21 @@ const array = [1,3,4,6,7,9,0,7,7,8,9,9,0,0]
 for (let i in array){
     console.log(array[i])
 }
+
+
+
+const numbers = [12,3445,8997,9,0,9];
+
+for (let i in numbers){
+    console.log(numbers[i])
+}
+/*let txt = "";*/
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array){
+    value = 0
+    index = 0
+    array = 0
+    console.log(value)
+}
+myFunction()
