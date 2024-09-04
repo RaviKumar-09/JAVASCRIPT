7757b33d8fe36ee338e1ad970d9d1e665b54ede5
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-04 16:59:20 +0530

    This example finds all <p> elements:

--- DOM elements/elements.js
+++ DOM elements/elements.js
@@ -1,12 +1,12 @@
-let id = document.getElementById("intro");
+/*let id = document.getElementById("intro");
 id.innerHTML = "Finding HTML Element by Id";
-console.log(id)
+console.log(id)*/
 
 
 const para = document.getElementsByTagName("p");
 console.log(para);
 
-const demo = document.getElementById("demo");
+/*const demo = document.getElementById("demo");
 demo.innerHTML = " This example finds all 'P' elements" + para[0].innerHTML;
-console.log(demo)
+console.log(demo)*/
 
