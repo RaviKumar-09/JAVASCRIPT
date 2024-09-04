15867d356f428486df75e86db3f5764e663cd75a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-04 16:53:11 +0530

    This example finds all <p> elements:

--- DOM elements/elements.js
+++ DOM elements/elements.js
@@ -6,3 +6,7 @@ console.log(id)
 let para = document.getElementsByTagName("p");
 console.log(para);
 
+let demo = document.getElementById("demo");
+demo.innerHTML = " This example finds all 'P' elements";
+console.log(demo)
+
